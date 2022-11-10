e85e4d80f2a3c7de4eca42018c2022779a14e962
Himanshu Sharma <hs6067308@gmail.com>
2022-11-10 15:12:14 +0530

    initial commit

---
+++ Assignment1.java
@@ -0,0 +1,160 @@
+package com.ineuronlearn.main.Assignment;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		 //Assignment 1 Q1
+		
+		//int n = 6;
+		//int i,j;
+		//for(i=0; i<n; i++) {
+		//	
+		//	
+		//	
+//			//print I
+//			for(j=0;j<n;j++) {
+//				if(i==0&&j<n-1||i==n-1&&j<n-1||j==(n-1)/2) {
+//					System.out.print("*");
+//					
+//				}else {
+//					System.out.print(" ");
+//				}
+//			}
+		//	
+//			//print N
+//				for(j=0; j<n;j++) {
+//					if(j==0||i==j||j==n-1) {
+//			        System.out.print("*");
+//									}
+//									else {
+//										System.out.print(" ");
+//									}
+//									
+//								}
+//				
+//				//print E
+//				for(j=0; j<n;j++) {
+//					if(i==0&&j>=(n-1)/2||j==(n-1)/2||i==(n-1)/2&&j>=(n-1)/2||i==n-1&&j>=(n-1)/2) {
+//			        System.out.print("*");
+//									}
+//									else {
+//										System.out.print(" ");
+//									}
+//									
+//								}
+//				
+//				//print U
+//				for(j=0;j<n;j++) {
+//				if(j==(n-1)/2||i==n-1&&j>=(n-1)/2||j==n-1) {
+//					System.out.print("*");
+//				}
+//				else {
+//					System.out.print(" ");
+//				}
+//				
+//			}
+//				
+//				//print R
+//				for(j=0;j<n;j++) {
+//				if(j==(n-1)/2||i==0&&j>=(n-1)/2||j==n-1&&i<=(n-1)/2||i==(n-1)/2&&j>=(n-1)/2||i==j&&i>(n-1)/2&&j>(n-1)/2) {
+//					System.out.print("*");
+//				}
+//				else {
+//					System.out.print(" ");
+//				}
+//				
+//			}
+		//	
+//			//print O
+//			for(j=0;j<n;j++) {
+//				if(j==n/4||i==0&&j>=n/4&&j<(3*n)/4||i==n-1&&j>=n/4&&j<(3*n)/4||j==(3*n)/4 ){
+//					System.out.print("*");
+//				}
+//				else {
+//					System.out.print(" ");
+//				}
+//				
+//			}
+//				
+//				//print N
+//				for(j=0; j<n;j++) {
+//				if(j==0||i==j||j==n-1) {
+//		        System.out.print("*");
+//								}
+//								else {
+//									System.out.print(" ");
+//								}
+//								
+//							}
+//							System.out.println("");
+//						}
+
+				
+				//Assignment 1 Q2
+//				int n = 5;
+//				int i,j;
+//				for( i=1;i<n;i++)
+//				{
+//					for( j=1; j<n; j++) {
+//						System.out.print(i);		
+//					}
+//					System.out.println();
+//				}
+
+		        // Assignment 1 Q3
+		//int n = 14;
+		//int i,j;
+		//
+		//for(i=0; i<n; i++) {
+//			for(j=0; j<n; j++) {
+//				if(i==0||j==0||i+j<=(n-1)/2
+//				||j-i>=(n-1)/2||j==n-1||i==n-1) {
+//					System.out.print("*");
+//			}
+//			else {
+//				System.out.print(" ");
+//			}
+		//}
+//			System.out.println();
+		//}
+
+		       //Assignment 1 Q4
+		//int n = 14;
+		//int i,j;
+		//
+		//for(i=0; i<n; i++) {
+//			for(j=0; j<n; j++) {
+//				if(j==0&&i>=(n-1)/2||i==n-1
+//					||j==n-1&&i>=(n-1)/2||i-j>=(n-1)/2
+//					||i+j>=(n-1)+((n-1)/2)) {
+//					System.out.print("*");
+//			}
+//			else {
+//				System.out.print(" ");
+//			}
+		//}
+//			System.out.println();
+		//}
+				
+		       //Assignment 1 Q5
+//				int n = 14;
+//				int i,j;
+		//
+//				for( i=0; i<n; i++) {
+//					for( j=0; j<n; j++) {
+//						if(i==0||j==0||i==n-1||i+j<=(n-1)/2||i-j>=(n-1)/2) {
+//							System.out.print("*");
+//					}
+//					else {
+//						System.out.print(" ");
+//					}
+//				}
+//					System.out.println();
+//				}
+
+
+
+	}
+
+}
